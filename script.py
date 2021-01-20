c42d04ec01dbec5336cdc6b8da5fc6051beb8cad
Sp <41648020+TheoJSP@users.noreply.github.com>
2021-01-20 06:33:21 -0300

    Publicacion de 3 fotos y mensaje que quieras

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 import pyautogui 
 import time 
 
+f = open('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/texto.txt','r')
+post = (f.read())
+
 groups = ['4053991651383281']
+
+imagenes = ['C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/p1.png', 
+'C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/p2.png', 
+'C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/p3.png' ]
 
 time.sleep(5)
 pyautogui.keyDown('ctrl')
@@ -19,20 +26,31 @@ for i in range(len(groups)):
 
     print("Publicando ...")
     try:
-        pyautogui.click('C:/Users/theot/Desktop/In process/FacebookPost/Img/b.png')
+        pyautogui.click('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/b.png') # Boton post
+        time.sleep(2)
+        pyautogui.typewrite(post) # Texto Post
         time.sleep(4)
-        pyautogui.typewrite("Prueba 2")
-        time.sleep(4)
-        pyautogui.click('C:/Users/theot/Desktop/In process/FacebookPost/Img/bimg.png')
-        time.sleep(6)
-        pyautogui.click('C:/Users/theot/Desktop/In process/FacebookPost/Img/nom.png')
-        time.sleep(6)
-        pyautogui.click('C:/Users/theot/Desktop/In process/FacebookPost/Img/env.png')
-        time.sleep(6)
-        pyautogui.click('C:/Users/theot/Desktop/In process/FacebookPost/Img/btn2.png')
+        pyautogui.click('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/bimg.png') # Btn img
+        time.sleep(3)
+
+        for i in range(len(imagenes)):
+            try:
+                pyautogui.click('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/bimg2.png') # Btn img
+                time.sleep(3)
+                pyautogui.click(imagenes[i]) # Img
+                time.sleep(3)
+                pyautogui.click('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/env.png') # Enviar
+                time.sleep(3)
+            except:
+                pyautogui.click(imagenes[i]) # Img
+                time.sleep(3)
+                pyautogui.click('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/env.png') # Enviar
+                time.sleep(3)
+
+        pyautogui.click('C:/Users/theot/Desktop/Cosas/Programacion/Python/FacebookPost/Img/btn2.png') # Publicar
         time.sleep(3)
         pyautogui.write(['f6'])
         time.sleep(3)
     except TypeError:
-        print("Fatal error!")
+        print("Fatal error! \n")
    
